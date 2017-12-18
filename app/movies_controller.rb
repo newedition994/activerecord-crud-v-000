eee0ad79dfd25b27e1300e48104a086a7bd96dc1
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-18 18:16:14 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -45,7 +45,7 @@ def can_find_the_first_item_from_the_database_using_id
 end
 
 def can_find_by_multiple_attributes
-  Movie.find_by(:title => "Title")
+  Movie.find_by(:title => "Title", :release_date => 2000, :director => "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
